e0404e7550bc044431813a53cfdc3e74c995218d
518Jerry <1685435788@qq.com>
2025-12-15 22:14:35 +0800

    Create display_numbers.py

---
+++ display_numbers.py
@@ -0,0 +1,13 @@
+from microbit import *
+
+while True:
+    display.show(Image("00900:"
+                       "00900:"
+                       "00900:"
+                       "00900:"
+                       "00900"))
+    sleep(1000)
+    display.show("2")
+    sleep(1000)
+    display.show("3")
+    sleep(1000)
